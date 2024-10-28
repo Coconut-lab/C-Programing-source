69970cb3a5e8aab31fa157ed2be635a4b786e56c
2coconut <51150721+Coconut-lab@users.noreply.github.com>
2024-10-28 15:20:51 +0900

    Update 06-MID.c

--- Ready_to_exam/MID_TEST/06-MID.c
+++ Ready_to_exam/MID_TEST/06-MID.c
@@ -4,7 +4,14 @@ int main(void) {
     double num1, num2, result;
     
     while (1) {
-        
+        static int count;
+
+        // 출력하는데 얼마나 걸리는지 출력 하는 문제
+        for (int i = 0; i < MAX; i++) {
+            fprintf(stdout, "i값 출력 %d\n", i)
+        }
+
+        /*
         information();  // ÇÔ¼öÀÇ È£Ãâ(00 ÇüÅÂ)
         num1 = input();
         num2 = input();
@@ -12,6 +19,8 @@ int main(void) {
         result = divide(num1, num2);
 
         output(result);
+        */
+        
     }
 
     return 0;
